7a909c7e3004ca224a366d5b46806a49676ad851
Vladyslav Moskalenko <vladkiew15@gmail.com>
2023-11-22 22:08:43 +0100

    test toggle fix

--- test/test_toggle_3x2.py
+++ test/test_toggle_3x2.py
@@ -22,7 +22,8 @@ class TestOnToggle(unittest.TestCase):
         self.assertEqual(result, expected)
 
     def test_right_top_edge(self):
-        size_row = 3
+        size_row = 2
+        size_column = 3
         initial_field = [0] * 6
 
         expected = [
@@ -30,12 +31,13 @@ class TestOnToggle(unittest.TestCase):
             0, 0, 1
         ]
 
-        toggler = LightToggler(size_row,2)
+        toggler = LightToggler(size_row, size_column)
         result = toggler.on_toggle(2, initial_field)
         self.assertEqual(result, expected)
 
     def test_right_bottom_edge(self):
-        size_row = 3
+        size_row = 2
+        size_column = 3
         initial_field = [0] * 6
 
         expected = [
@@ -43,12 +45,13 @@ class TestOnToggle(unittest.TestCase):
             0, 1, 1
         ]
 
-        toggler = LightToggler(size_row,2)
+        toggler = LightToggler(size_row, size_column)
         result = toggler.on_toggle(5, initial_field)
         self.assertEqual(result, expected)
 
     def test_left_bottom_edge(self):
-        size_row = 3
+        size_row = 2
+        size_column = 3
         initial_field = [0] * 6
 
         expected = [
@@ -56,12 +59,13 @@ class TestOnToggle(unittest.TestCase):
             1, 1, 0
         ]
 
-        toggler = LightToggler(size_row,2)
+        toggler = LightToggler(size_row, size_column)
         result = toggler.on_toggle(3, initial_field)
         self.assertEqual(result, expected)
 
     def test_middle_top(self):
-        size_row = 3
+        size_row = 2
+        size_column = 3
         initial_field = [0] * 6
 
         expected = [
@@ -69,12 +73,13 @@ class TestOnToggle(unittest.TestCase):
             0, 1, 0
         ]
 
-        toggler = LightToggler(size_row,2)
+        toggler = LightToggler(size_row, size_column)
         result = toggler.on_toggle(1, initial_field)
         self.assertEqual(result, expected)
 
     def test_middle_bottom(self):
-        size_row = 3
+        size_row = 2
+        size_column = 3
         initial_field = [0] * 6
 
         expected = [
@@ -82,7 +87,7 @@ class TestOnToggle(unittest.TestCase):
             1, 1, 1
         ]
 
-        toggler = LightToggler(size_row,2)
+        toggler = LightToggler(size_row, size_column)
         result = toggler.on_toggle(4, initial_field)
         self.assertEqual(result, expected)
 
